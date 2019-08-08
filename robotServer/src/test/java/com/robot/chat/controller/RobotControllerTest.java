ab7a341e68aa49b60b3e10852aee5ed8f0b333e8
zhuchenshu <1369359429@qq.com>
2019-08-08 14:29:35 +0800

    增加文档

--- robotServer/src/test/java/com/robot/chat/controller/RobotControllerTest.java
+++ robotServer/src/test/java/com/robot/chat/controller/RobotControllerTest.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
